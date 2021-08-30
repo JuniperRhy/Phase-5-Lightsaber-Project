96277a453fd419b514b9bad581641c1e4bbc615f
JuniperRhy <gwyniver.rhy@gmail.com>
2021-08-30 17:36:09 -0500

    Saber Test

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,193 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="saber">
+      <div className="grip">
+        <svg
+          width="281"
+          height="147"
+          viewBox="0 0 281 147"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
         >
-          Learn React
-        </a>
-      </header>
+          <rect
+            x="0.5"
+            y="12.5"
+            width="280"
+            height="120"
+            className="grip"
+            // fill="#706F6F"
+            fill="#b36c31"
+            stroke="black"
+            stroke-width="2"
+          />
+          <rect
+            x="19"
+            y="3"
+            width="19"
+            height="143"
+            fill="#C4C4C4"
+            stroke="black"
+            stroke-width="2"
+          />
+          <rect
+            x="57"
+            y="3"
+            width="19"
+            height="143"
+            fill="#C4C4C4"
+            stroke="black"
+            stroke-width="2"
+          />
+          <rect
+            x="95"
+            y="3"
+            width="19"
+            height="143"
+            fill="#C4C4C4"
+            stroke="black"
+            stroke-width="2"
+          />
+          <rect
+            x="135"
+            y="1"
+            width="19"
+            height="143"
+            fill="#C4C4C4"
+            stroke="black"
+            stroke-width="2"
+          />
+          <rect
+            x="174"
+            y="3"
+            width="19"
+            height="143"
+            fill="#C4C4C4"
+            stroke="black"
+            stroke-width="2"
+          />
+          <rect
+            x="212"
+            y="1"
+            width="19"
+            height="143"
+            fill="#C4C4C4"
+            stroke="black"
+            stroke-width="2"
+          />
+        </svg>
+      </div>
+      <div className="switch">
+        <svg
+          width="281"
+          height="141"
+          viewBox="0 0 281 141"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect
+            className="switch"
+            x="1"
+            y="21"
+            width="279"
+            height="119"
+            fill="#a05601"
+            stroke="black"
+            stroke-width="2"
+          />
+          <rect
+            x="49"
+            y="1"
+            width="144"
+            height="56"
+            fill="#3d3d3d"
+            stroke="black"
+            stroke-width="2"
+          />
+          <rect
+            x="59"
+            y="1"
+            width="123"
+            height="27"
+            fill="#ffbb00"
+            stroke="black"
+            stroke-width="2"
+          />
+          <line x1="70.5" x2="70.5" y2="29" stroke="black" />
+          <line x1="81.5" x2="81.5" y2="29" stroke="black" />
+          <line
+            y1="-0.5"
+            x2="29"
+            y2="-0.5"
+            transform="matrix(0 1 1 0 94 0)"
+            stroke="black"
+          />
+          <line x1="105.5" x2="105.5" y2="29" stroke="black" />
+          <line x1="116.5" x2="116.5" y2="29" stroke="black" />
+          <line
+            y1="-0.5"
+            x2="29"
+            y2="-0.5"
+            transform="matrix(0 1 1 0 129 0)"
+            stroke="black"
+          />
+          <line x1="141.5" x2="141.5" y2="29" stroke="black" />
+          <line x1="152.5" x2="152.5" y2="29" stroke="black" />
+          <line
+            y1="-0.5"
+            x2="29"
+            y2="-0.5"
+            transform="matrix(0 1 1 0 165 0)"
+            stroke="black"
+          />
+          <rect
+            x="227"
+            y="21"
+            width="18"
+            height="119"
+            fill="#000000"
+            stroke="black"
+            stroke-width="2"
+          />
+          <rect
+            x="15"
+            y="21"
+            width="23"
+            height="119"
+            fill="#000000"
+            stroke="black"
+            stroke-width="2"
+          />
+        </svg>
+      </div>
+      <div className="emitter">
+        <svg
+          width="404"
+          height="272"
+          viewBox="0 0 404 272"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect
+            x="1"
+            y="75"
+            width="277"
+            height="119"
+            fill="#706F6F"
+            className="emitter"
+            stroke="black"
+            stroke-width="2"
+          />
+          <path
+            d="M315.756 69.3274L318.595 200.933C318.778 209.398 309.727 214.887 302.304 210.813L186.911 147.469C179.488 143.394 179.26 132.812 186.5 128.421L299.054 60.1592C306.295 55.7681 315.573 60.8616 315.756 69.3274Z"
+            fill="#C4C4C4"
+            stroke="black"
+            stroke-width="2"
+          />
+        </svg>
+      </div>
     </div>
   );
 }
